d4601908b8130beab1bd48a989fd05f9d98bc743
solonovamax <solonovamax@12oclockpoint.com>
2019-11-26 22:49:21 -0500

    Allowed for usage of either X or Z axis on the joystick for robot control.

--- src/main/java/frc/robot/commands/DriveInput.java
+++ src/main/java/frc/robot/commands/DriveInput.java
@@ -8,26 +8,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
 import frc.robot.Robot;
 
 
 /**
  * An example command.  You can replace me with your own command.
  */
-public class ExampleCommand extends Command {
-	public ExampleCommand() {
+public class DriveInput extends Command {
+	
+	public DriveInput() {
 		// Use requires() here to declare subsystem dependencies
-		requires(Robot.m_subsystem);
+		requires(Robot.driveTrainSubsystem);
 	}
 	
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
+	
 	}
 	
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
+		Robot.driveTrainSubsystem.arcadeDrive(OI.driverController.getY(), (Math.abs(OI.driverController.getX()) > Math.abs(OI.driverController.getZ()) ? OI.driverController.getX() : OI.driverController.getZ()));
 	}
 	
 	// Make this return true when this Command no longer needs to run execute()
@@ -39,11 +43,14 @@ public class ExampleCommand extends Command {
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
+		Robot.driveTrainSubsystem.arcadeDrive(0, 0);
 	}
 	
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	@Override
 	protected void interrupted() {
+		Robot.driveTrainSubsystem.arcadeDrive(0, 0);
 	}
+	
 }
